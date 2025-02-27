c0c49f1a4f7cc826ec4810d6061d4075e77d264c
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-02-27 10:25:58 +0700

    setup userchat
    view folder 2 file

--- Program.cs
+++ Program.cs
@@ -41,4 +41,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "userchat",
+    pattern: "UserChat/{action}/{id?}",
+    defaults: new { controller = "UserChat", action = "Index" });
+
 app.Run();
