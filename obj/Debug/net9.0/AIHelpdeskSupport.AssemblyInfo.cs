203e15664a0b119a03205de2d81cbf40d11ac6ce
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-02-28 11:05:35 +0700

    error chatbox

--- obj/Debug/net9.0/AIHelpdeskSupport.AssemblyInfo.cs
+++ obj/Debug/net9.0/AIHelpdeskSupport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba86ba5705252a625adaf0b4b6684dd6d4b201c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc0a576db6a1f87abd5af4b49db95003bc1b223")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
