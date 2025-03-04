5f1725f78ffb349000d3487a73aa17e0a2cf3bf4
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-03-04 13:38:00 +0700

    user edit

--- obj/Debug/net9.0/AIHelpdeskSupport.AssemblyInfo.cs
+++ obj/Debug/net9.0/AIHelpdeskSupport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380b1363bda99e8dda430bd973ed3a5d72e3393c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4edd9b077689efbc4cd07498b265af0cff21981f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
