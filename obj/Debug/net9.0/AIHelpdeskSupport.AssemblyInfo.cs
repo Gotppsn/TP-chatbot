0376c60f8b354c96661cd62538a8d01ab40b2d02
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-03-11 10:20:07 +0700

    ChatBot UI

--- obj/Debug/net9.0/AIHelpdeskSupport.AssemblyInfo.cs
+++ obj/Debug/net9.0/AIHelpdeskSupport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69acee23f792850e7c2c766327f1e23542d06806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10619e6d65dc535c828ac8551f90ba062fbdbd82")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIHelpdeskSupport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
